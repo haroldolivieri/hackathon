f3e8cde3ae2a4200844c45ba03e3039efc26f72d
Giovanni keppelen <keppelen.as@gmail.com>
2015-08-16 18:30:50 -0300

    rename folders

--- 2015/Pvc Online/app/controllers/VideoController.js
+++
@@ -1,5 +0,0 @@
-app.controller('VideoController', function ($routeParams,$sce){
-	Video = this;
-	Video.id = $routeParams.id;
-	Video.url = $sce.trustAsResourceUrl("http://www.youtube.com/embed/"+ Video.id +"?autoplay=1"); 
-});
